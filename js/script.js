14c4e42f68074ba3a49c6f78a3929672d2b6b394
Mohd Ebrahim <mohd.ebrahim.jmi@gmail.com>
2024-01-31 19:43:48 +0530

    my first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function () {
+  jQuery('.slide-items').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+  })
+})
