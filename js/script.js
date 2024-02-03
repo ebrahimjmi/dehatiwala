4fb5a70530db968e3d5fade9e6fcbf062e8febdd
Mohd Ebrahim <mohd.ebrahim.jmi@gmail.com>
2024-02-03 17:42:27 +0530

    New and noteworthy, Most booked services, Salone for Women, Spa for women section done

--- js/script.js
+++ js/script.js
@@ -1,10 +1,50 @@
 jQuery(document).ready(function () {
-  jQuery('.slide-items').slick({
+  jQuery(".banner-slider").slick({
     dots: true,
-    infinite: false,
     speed: 300,
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
-  })
-})
+  });
+  jQuery(".service-slider .slider").slick({
+    autoplay: false,
+    infinite: false,
+    autoplaySpeed: 1500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: false,
+    arrows: true,
+    focusOnSelect: true,
+    prevArrow:
+      '<div class="slide-arrow prev-arrow"><i class="fa-solid fa-chevron-left"></i></div>',
+    nextArrow:
+      '<div class="slide-arrow next-arrow"><i class="fa-solid fa-chevron-right"></i></div> <div class="shop-btn" style="margin-left: 10px;"></div>',
+  });
+
+  jQuery(".women-salone .slider").slick({
+    autoplay: false,
+    autoplaySpeed: 1500,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    dots: false,
+    arrows: true,
+    focusOnSelect: false,
+    prevArrow:
+      '<div class="slide-arrow prev-arrow"><i class="fa-solid fa-chevron-left"></i></div>',
+    nextArrow:
+      '<div class="slide-arrow next-arrow"><i class="fa-solid fa-chevron-right"></i></div> <div class="shop-btn" style="margin-left: 10px;"></div>',
+  });
+  jQuery(".notworthy .slider").slick({
+    autoplay: false,
+    autoplaySpeed: 1500,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    dots: false,
+    arrows: true,
+    focusOnSelect: false,
+    prevArrow:
+      '<div class="slide-arrow prev-arrow"><i class="fa-solid fa-chevron-left"></i></div>',
+    nextArrow:
+      '<div class="slide-arrow next-arrow"><i class="fa-solid fa-chevron-right"></i></div> <div class="shop-btn" style="margin-left: 10px;"></div>',
+  });
+});
